4a1135b2f352c69c7e84368008b480acca67e93e
prajapatideepesh11 <52192861+prajapatideepesh11@users.noreply.github.com>
2022-02-20 07:29:57 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,3 +6,18 @@ resource "aws_s3_bucket" "mybucket" {
     Environment = "Dev"
   }
 }
+provider "aws" {
+  access_key = "${var.access_key}"
+   secret_key = "${var.secret_key}"
+   region = "${var.region}"
+}
+variable "access_key"{
+  default = "AKIA4WYCBYUNNAHFREMR"
+}
+
+variable "secret_key"{
+  default = "IEEWM62LrUne8hD346BqRHlv7f7E1DjwdYfpZOrV"
+}
+variable "region"{
+  default = "ap-south-1"
+}
