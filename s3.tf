b0efcf4e66cd999ef5317546a2460b43857cff5f
prajapatideepesh11 <52192861+prajapatideepesh11@users.noreply.github.com>
2022-02-25 05:58:29 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,13 @@
-Ok
+provider "aws" {
+  region ="ap-south-1"
+}
+
+
+resource "aws_s3_bucket" "mybucket" {
+    bucket = "my-deepesh-tf-using-jenkins"
+
+tags={
+    Name ="My bucket"
+    Environment ="Dev"
+}  
+}
